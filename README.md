ac6c7e43e0afafcd2c3a0dab665f17116eba9994
AXiX-official <2879710747@qq.com>
2025-09-04 07:39:00 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # clice-zed
 
 This is the zed extension for [clice](https://github.com/clice-project/clice).
+
+## installation
+
+Currently, `clice-zed` can only be installed manually through `Install Dev Extension`.
+
+to install `clice-zed`, run the following command:
+
+```
+git clone https://github.com/AXiX-official/clice-zed.git
+```
+
+Then in the extensions page, click the `Install Dev Extension` button (or the `zed: install dev extension` action) and select the directory `clice-zed`.
--- src/clice.rs
+++ src/clice.rs
@@ -10,22 +10,27 @@ struct CliceBinary {
 impl CliceExtension {
     fn find_clice_binary(&self, worktree: &Worktree) -> Result<CliceBinary> {
         if let Some(path_str) = worktree.which("clice") {
+            // The std::path module seems to behave unexpectedly in the Zed sandbox,
+            // failing to correctly parse parent directories for full paths on Windows.
+            // To ensure reliability, we revert to manual string manipulation to find the parent directory.
             let separator_pos = path_str.rfind(|c| c == '\\' || c == '/');
 
             if let Some(pos) = separator_pos {
                 let parent_dir = &path_str[..pos];
-                let resource_dir = format!("{}\\{}", parent_dir, "lib");
+                // We use std::path::MAIN_SEPARATOR to construct the path in a cross-platform way,
+                // avoiding the hardcoded '\' from the original implementation.
+                let resource_dir = format!("{}{}{}", parent_dir, std::path::MAIN_SEPARATOR, "lib");
 
                 Ok(CliceBinary {
                     path: path_str,
                     resource_dir,
                 })
             } else {
-                return Err(format!(
+                Err(format!(
                     "clice found as `{}`,
                     but a full path is required to find the `lib` directory.",
                     path_str
-                ));
+                ))
             }
         } else {
             Err(
@@ -40,6 +45,7 @@ impl zed::Extension for CliceExtension {
         Self
     }
 
+    // Currently, we only search for the 'clice' binary in the system's PATH.
     fn language_server_command(
         &mut self,
         _language_server_id: &LanguageServerId,
